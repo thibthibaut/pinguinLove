3ce5ee025c320f58399d0a4972d7acf73a0f134f
Thibaut <thibaut@MacBook-Pro-de-Thibaut.local>
2018-04-29 15:34:59 +0200

    Fixed some bug

--- main.js
+++ main.js
@@ -1,14 +1,39 @@
-var GRID_SIZE=8;
-var PINGU_NBR=10;
+var GRID_SIZE = 4;
+var Y_OFFSET = 50;
 var w = 50;
+var grid_size_jump = [5, 10, 20, 30, 40, 50, 50, 50, 60, 60, 100, 100];
+var levelNumber = 0;
+var levelMinorCounter = 0;
 
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {
+    document.cookie = name + '=; Max-Age=-99999999;';
+}
 
 function createGrid(rows) {
     var arr = [];
 
-    for (var i=0;i<rows;i++) {
+    for (var i = 0; i < rows; i++) {
         arr[i] = [];
-        for (var j=0;j<rows;j++) {
+        for (var j = 0; j < rows; j++) {
             arr[i].push(new Cell());
         }
     }
@@ -22,41 +47,47 @@ function getRandomItem(set) {
 }
 
 
-function Cell(){
+function Cell() {
 
     this.content = 'EMPTY';
     this.placed_content = 'EMPTY';
 
-    this.toggleContent = function(){
-        switch(this.placed_content){
+    this.toggleContent = function () {
+        switch (this.placed_content) {
             case 'EMPTY':
-                this.placed_content= 'GRASS';
+                this.placed_content = 'GRASS';
                 break;
             case 'GRASS':
-                this.placed_content= 'PLACED_PINGU';
+                this.placed_content = 'PLACED_PINGU';
                 break;
             case 'PLACED_PINGU':
-                this.placed_content= 'EMPTY';
+                this.placed_content = 'EMPTY';
                 break;
         }
     }
 
 };
 
-function Board(){
+function Board() {
 
     this.grid = createGrid(GRID_SIZE);
+    this.rowHeader = Array(GRID_SIZE).fill(0);
+    this.colHeader = Array(GRID_SIZE).fill(0);
+    this.isRowValid = Array(GRID_SIZE).fill(false);
+    this.isColValid = Array(GRID_SIZE).fill(false);
 
-    this.generateGame = function(){
+    this.isWon = false;
+
+    this.generateGame = function () {
 
         // Create set of available positions
         availablePositions = new Set();
-        for(var i = 0; i < GRID_SIZE*GRID_SIZE; i++)
+        for (var i = 0; i < GRID_SIZE * GRID_SIZE; i++)
             availablePositions.add(i);
 
 
         var timeOut = 0;
-        while(availablePositions.size > 0){
+        while (availablePositions.size > 0) {
 
             randomPos = getRandomItem(availablePositions);
 
@@ -68,134 +99,296 @@ function Board(){
 
             // Place a tibou next to the pingu
             var isTibouPlaced = false;
-            while( !isTibouPlaced ){
-               let tibouPos =  Math.floor(Math.random()*4);
-               switch(tibouPos){
-                   case 0:
-                        if(x-1>=0 && this.grid[x-1][y].content == 'EMPTY' ){
-                            this.grid[x-1][y].placed_content = 'TIBOU';
+            while (!isTibouPlaced) {
+                let tibouPos = Math.floor(Math.random() * 4);
+                switch (tibouPos) {
+                    case 0:
+                        if (x - 1 >= 0 && this.grid[x - 1][y].content == 'EMPTY') {
+                            this.grid[x - 1][y].content = 'TIBOU';
+                            this.grid[x - 1][y].placed_content = 'TIBOU';
                             isTibouPlaced = true;
                         }
                         break;
-                   case 1: if(x+1<GRID_SIZE && this.grid[x+1][y].content == 'EMPTY' ){
-                            this.grid[x+1][y].placed_content = 'TIBOU';
+                    case 1: if (x + 1 < GRID_SIZE && this.grid[x + 1][y].content == 'EMPTY') {
+                        this.grid[x + 1][y].content = 'TIBOU';
+                        this.grid[x + 1][y].placed_content = 'TIBOU';
+                        isTibouPlaced = true;
+                    }
+                        break;
+                    case 2:
+                        if (y - 1 >= 0 && this.grid[x][y - 1].content == 'EMPTY') {
+                            this.grid[x][y - 1].content = 'TIBOU';
+                            this.grid[x][y - 1].placed_content = 'TIBOU';
                             isTibouPlaced = true;
                         }
                         break;
-                   case 2:
-                        if(y-1>=0 && this.grid[x][y-1].content == 'EMPTY' ){
-                            this.grid[x][y-1].placed_content = 'TIBOU';
+                    case 3:
+                        if (y + 1 < GRID_SIZE && this.grid[x][y + 1].content == 'EMPTY') {
+                            this.grid[x][y + 1].content = 'TIBOU';
+                            this.grid[x][y + 1].placed_content = 'TIBOU';
                             isTibouPlaced = true;
                         }
                         break;
-                   case 3:
-                        if(y+1<GRID_SIZE && this.grid[x][y+1].content == 'EMPTY' ){
-                            this.grid[x][y+1].placed_content = 'TIBOU';
-                            isTibouPlaced = true;
-                        }
-                        break;
-               }
-               if(++timeOut > 100000){
-                   alert("impossible to create grid");
-               }
-               
+                }
+                if (++timeOut > 100000) {
+                    alert("impossible to create grid");
+                }
+
             }
-
-
 
 
             availablePositions.delete(randomPos);
 
             //Remove all 8 neigbours from set so we won't pick them next time
-            availablePositions.delete(  (x-1) + (y-1)*GRID_SIZE);
-            availablePositions.delete(  (x+0) + (y-1)*GRID_SIZE);
-            availablePositions.delete(  (x+1) + (y-1)*GRID_SIZE);
-            availablePositions.delete(  (x+1) + (y+0)*GRID_SIZE);
-            availablePositions.delete(  (x+1) + (y+1)*GRID_SIZE);
-            availablePositions.delete(  (x+0) + (y+1)*GRID_SIZE);
-            availablePositions.delete(  (x-1) + (y+1)*GRID_SIZE);
-            availablePositions.delete(  (x-1) + (y+0)*GRID_SIZE);
+            availablePositions.delete((x - 1) + (y - 1) * GRID_SIZE);
+            availablePositions.delete((x + 0) + (y - 1) * GRID_SIZE);
+            availablePositions.delete((x + 1) + (y - 1) * GRID_SIZE);
+            availablePositions.delete((x + 1) + (y + 0) * GRID_SIZE);
+            availablePositions.delete((x + 1) + (y + 1) * GRID_SIZE);
+            availablePositions.delete((x + 0) + (y + 1) * GRID_SIZE);
+            availablePositions.delete((x - 1) + (y + 1) * GRID_SIZE);
+            availablePositions.delete((x - 1) + (y + 0) * GRID_SIZE);
 
             console.log(availablePositions);
         }
 
-        console.log("GRID");
-        console.log(this.grid);
+        console.table(this.grid);
+        // Fill in Header
 
-    };
-
-    this.display = function(){
-
-        //background(0);
-        for(let row=0; row< GRID_SIZE; row++){
-            for(let col=0; col< GRID_SIZE; col++){
-
-                fill(255);
-                switch(this.grid[row][col].placed_content){
-                    case 'TIBOU':
-                    image(tibouImg, row*w, col*w, w, w);
-                    break;
-                    case 'PLACED_PINGU':
-                    console.log("COUCOU");
-                    image(pinguImg, row*w, col*w, w, w);
-                    break;
-                    case 'GRASS':
-                    image(grassImg, row*w, col*w, w, w);
-                    break;
-                    case 'EMPTY':
-                    image(emptyImg, row*w, col*w, w, w);
-                    break;
+        let pCounter = 0;
+        let tCounter = 0;
+        for (let i = 0; i < GRID_SIZE; i++) {
+            for (let j = 0; j < GRID_SIZE; j++) {
+                if (this.grid[i][j].content == 'PINGU') {
+                    pCounter++;
+                    this.colHeader[i]++;
+                    this.rowHeader[j]++;
                 }
-//                rect(row*w, col*w, w, w, 0);
-
+                if( this.grid[i][j].content == 'TIBOU' ){
+                    tCounter++;
+                }
             }
         }
+        if( pCounter != tCounter ) alert("FATAL ERROR IN GAME GENERATION");
+
+        console.log(this.rowHeader);
+        console.log(this.colHeader);
+
+
     };
 
+    this.display = function () {
+        background(13, 38, 35);
 
+        if (this.isWon) {
+
+            setFrameRate(10);
+
+            for (let row = 0; row < GRID_SIZE; row++) {
+                fill(Math.floor(random(128, 255)), Math.floor(random(128, 255)), Math.floor(random(128, 255)));
+                text(Math.floor(random(0, 9)), -w / 3, row * w + w / 2 + w / 8);
+
+                for (let col = 0; col < GRID_SIZE; col++) {
+
+                    switch (Math.floor(random(0, 3))) {
+                        case 0:
+                            image(tibouImg, row * w, col * w, w, w);
+                            break;
+                        case 1:
+                            image(pinguImg, row * w, col * w, w, w);
+                            break;
+                        case 2:
+                            image(grassImg, row * w, col * w, w, w);
+                            break;
+                        case 3:
+                            image(emptyImg, row * w, col * w, w, w);
+                            break;
+                    }
+                }
+            }
+
+            for (let col = 0; col < GRID_SIZE; col++) {
+                fill(Math.floor(random(128, 255)), Math.floor(random(128, 255)), Math.floor(random(128, 255)));
+                text(Math.floor(random(10)), col * w + w / 2 - w / 8, GRID_SIZE * w + w / 2);
+            }
+            fill(255)
+            textSize(w / 2);
+            text('You won. Congratulations', 0, Y_OFFSET, GRID_SIZE * w, GRID_SIZE * w);
+            textSize(w / 2);
+            text('Next grid >>', 1 * w, Y_OFFSET + 2 * w, GRID_SIZE * w, GRID_SIZE * w);
+            textSize(30);
+
+        } else {
+
+            for (let row = 0; row < GRID_SIZE; row++) {
+                fill(246, 228, 240);
+                if (this.isRowValid[row]) fill(87, 199, 102);
+
+                text(this.rowHeader[row], -w / 3, row * w + w / 2 + w / 8);
+
+                for (let col = 0; col < GRID_SIZE; col++) {
+
+                    switch (this.grid[row][col].placed_content) {
+                        case 'TIBOU':
+                            image(tibouImg, row * w, col * w, w, w);
+                            break;
+                        case 'PLACED_PINGU':
+                            image(pinguImg, row * w, col * w, w, w);
+                            break;
+                        case 'GRASS':
+                            image(grassImg, row * w, col * w, w, w);
+                            break;
+                        case 'EMPTY':
+                            image(emptyImg, row * w, col * w, w, w);
+                            break;
+                    }
+                    //                rect(row*w, col*w, w, w, 0);
+
+                }
+            }
+
+            for (let col = 0; col < GRID_SIZE; col++) {
+                fill(246, 228, 240);
+                if (this.isColValid[col]) fill(87, 199, 102);
+                text(this.colHeader[col], col * w + w / 2 - w / 8, GRID_SIZE * w + w / 2);
+
+            }
+
+        }
+
+    };
+
+    this.check4TheWin = function () {
+
+        for (let row = 0; row < GRID_SIZE; row++) {
+            let pinguCounter = 0;
+            let grassCounter = 0;
+            let tibouCounter = 0;
+            for (let col = 0; col < GRID_SIZE; col++) {
+                if (this.grid[col][row].placed_content == 'PLACED_PINGU') pinguCounter++;
+                if (this.grid[col][row].placed_content == 'GRASS') grassCounter++;
+                if (this.grid[col][row].placed_content == 'TIBOU') tibouCounter++;
+            }
+            if (pinguCounter == this.rowHeader[row] && pinguCounter + grassCounter + tibouCounter == GRID_SIZE) {
+                this.isRowValid[row] = true;
+            } else {
+                this.isRowValid[row] = false;
+            }
+        }
+
+        for (let col = 0; col < GRID_SIZE; col++) {
+            let pinguCounter = 0;
+            let grassCounter = 0;
+            let tibouCounter = 0;
+            for (let row = 0; row < GRID_SIZE; row++) {
+                if (this.grid[col][row].placed_content == 'PLACED_PINGU') pinguCounter++;
+                if (this.grid[col][row].placed_content == 'GRASS') grassCounter++;
+                if (this.grid[col][row].placed_content == 'TIBOU') tibouCounter++;
+            }
+            if (pinguCounter == this.colHeader[col] && pinguCounter + grassCounter + tibouCounter == GRID_SIZE) {
+                this.isColValid[col] = true;
+            } else {
+                this.isColValid[col] = false;
+            }
+        }
+
+        var isAllValid = true;
+        for (let i = 0; i < GRID_SIZE; i++) {
+            if (!this.isRowValid[i]) isAllValid = false;
+            if (!this.isColValid[i]) isAllValid = false;
+        }
+
+        if (isAllValid) {
+            console.log("ALL VALID. verification now");
+
+            // Check if all pingus are on placed pingus
+            var isAllPinguMatch = true;
+            for (let row = 0; row < GRID_SIZE; row++) {
+                for (let col = 0; col < GRID_SIZE; col++) {
+                    if (this.grid[row][col].placed_content == 'PLACED_PINGU' && this.grid[row][col].content != 'PINGU') {
+                        isAllPinguMatch = false;
+                    }
+                }
+            }
+            if (isAllPinguMatch == true) this.isWon = true;
+
+
+
+
+        }
+
+    };
 };
 
 
 board = new Board();
 
 function preload() {
-    tibouImg = loadImage('images/tibou.png');
-    pinguImg = loadImage('images/pingu.png');
+    tibouImg = loadImage('images/tibou_.png');
+    pinguImg = loadImage('images/pingu_.png');
     grassImg = loadImage('images/grass.png');
     emptyImg = loadImage('images/empty.png');
-  }
+    robotoFnt = loadFont('fonts/Roboto-Light.ttf');
+}
 
-function setup(){
+function setup() {
 
+    var previousScore = getCookie('levelNum');
 
+    if (false && previousScore) {
+        levelNumber = previousScore;
+        levelMinorCounter = getCookie('levelMinorNum');
+        GRID_SIZE = getCookie('gridSize');
+        board = new Board();
+        alert("restored your session");
+        console.log('>>>>>>>>>>' + levelNumber + ' ' + levelMinorCounter + ' ' + GRID_SIZE);
+    }
     // createCanvas(800,800);
     createCanvas(windowWidth, windowHeight);
-    w = (windowHeight-40) /  GRID_SIZE;
+    w = min((windowHeight - Y_OFFSET) / (GRID_SIZE + 1), (windowWidth) / (GRID_SIZE + 1));
     //background(0);
-    console.log(w);
 
+    textSize(40);
+    textFont(robotoFnt);
     board.generateGame();
-
-//    board.display();
-
 
 }
 
 
-function draw(){
-    translate(windowWidth/2-(w*GRID_SIZE)/2, 0);
+function draw() {
+    translate(windowWidth / 2 - (w * GRID_SIZE) / 2, Y_OFFSET);
 
     board.display();
-
-//    ellipse(50, 50, 80, 80);
+    fill(246, 228, 240);
+    textSize(w / 6);
+    text('Level ' + levelNumber + ' - Size jump in ' + (grid_size_jump[GRID_SIZE - 4] - levelMinorCounter) + ' levels', 0, -5);
+    textSize(w / 3);
+    //    ellipse(50, 50, 80, 80);
 }
 
 function mouseClicked() {
-    let rectangleX = Math.floor((mouseX-(windowWidth/2-(w*GRID_SIZE)/2)) /w)
-    let rectangleY = Math.floor(mouseY/w);
-    console.log(rectangleX + ' ' + rectangleY);
-    if(rectangleX >= 0 && rectangleX < GRID_SIZE && rectangleY >= 0 && rectangleY < GRID_SIZE){
+    let rectangleX = Math.floor((mouseX - (windowWidth / 2 - (w * GRID_SIZE) / 2)) / w)
+    let rectangleY = Math.floor((mouseY - Y_OFFSET) / w);
+    // if(board.isWon && (rectangleX == GRID_SIZE-1 || rectangleX == GRID_SIZE -2) && (rectangleY == GRID_SIZE-1 || rectangleY == GRID_SIZE -2 )){
+    if (board.isWon) {
+        levelNumber++;
+        levelMinorCounter++;
+        setCookie('levelNum', levelNumber, 800);
+        setCookie('levelMinorNum', levelMinorCounter, 800);
+        setCookie('gridSize', GRID_SIZE, 800);
+        if (grid_size_jump[GRID_SIZE - 4] == levelMinorCounter) {
+            GRID_SIZE++;
+            levelMinorCounter = 0;
+            w = min((windowHeight - Y_OFFSET) / (GRID_SIZE + 1), (windowWidth) / (GRID_SIZE + 1));
+        }
+        board = new Board();
+        board.generateGame();
+        setFrameRate(30);
+    }
+    else if (rectangleX >= 0 && rectangleX < GRID_SIZE && rectangleY >= 0 && rectangleY < GRID_SIZE) {
         board.grid[rectangleX][rectangleY].toggleContent();
+        board.check4TheWin();
     }
 
-  }
+
+}
